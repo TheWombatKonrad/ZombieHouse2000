bde0275b6986abc3c3cc60e37fdf630ec4a44817
Malte <wombatconrad@gmail.com>
2025-01-15 16:09:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from math import inf
+from math import inf, ceil
 from os import system, name
 import random
 from time import sleep
@@ -14,17 +14,6 @@ def input_valid_int(question, error_text, min=0, max=inf):
         except ValueError:
             print(error_text, end=" ")
 
-def input_valid_float(question, error_text, min=0, max=inf):
-    while True:
-        try:
-            value = float(input(question))
-            if min <= value <= max:
-                return value
-            else:
-                print(error_text, end=" ")
-        except ValueError:
-            print(error_text, end=" ")
-
 def input_valid_str(question, error_text, possible_answers):
     answer = input(question)
     while answer not in possible_answers:
@@ -32,33 +21,36 @@ def input_valid_str(question, error_text, possible_answers):
 
     return answer
 
-#Clears the screen depending on the OS used.
+# Clears the screen depending on the OS used.
 def clear_screen():
     if name == "nt":
         _ = system("cls")
     else:
         _ = system("clear")
 
-#Returns a parameter that has been used less than the max amount.
+# Returns a parameter that has been used less than the max amount.
 def get_parameter(max_repeats, previous_numbers):
     while True:
         parameter = random.randint(0, 12)
         if previous_numbers.count(parameter) < max_repeats:
             return parameter
 
-def get_max_repeats(number_of_questions):
-    if number_of_questions > 26:
-        return 3
-    elif number_of_questions > 13:
-        return 2
-    else:
-        return 1
+# Runs the operation based on the assigned method.
+def run_math(method, constant_parameter, random_parameter):
+        if method == "*":    
+            return constant_parameter * random_parameter
 
-eaten_by_zombies = False
+        elif method == "//":
+            return random_parameter // constant_parameter
+
+        elif method == "%":
+            return random_parameter % constant_parameter
+
 continue_playing = True
+eaten_by_zombies = False
 
 while continue_playing:
-    #If true, the player has lost the previous round, and the previous conditions are repeated.
+    # If true, the player has lost the previous round, and the previous conditions are repeated.
     if not eaten_by_zombies:
         print("*********************************" +
           "\nZombie House 2000" +
@@ -84,67 +76,66 @@ while continue_playing:
     
     previous_numbers = []
     remaining_doors = number_of_questions
-    max_repeats = get_max_repeats(number_of_questions)
+    max_repeats = ceil(number_of_questions / 13)
+    # Resets the game if the player has already lost once.
+    eaten_by_zombies = False
     
     for i in range(number_of_questions):
         print(f"Question {i + 1} of {number_of_questions}:", end=" ")
 
-        #This is the factor or dividend.
+        # This is the factor or dividend.
         parameter = get_parameter(max_repeats, previous_numbers)
         previous_numbers.append(parameter)
 
-        #Multiplication
+        # Multiplication
         if method == "*":    
-            answer = input_valid_int(f"{table} * {parameter} = ", "Please enter a number. ")
+            user_answer = input_valid_int(f"{table} * {parameter} = ", "Please enter a number. ")
+            correct_answer = run_math("*", table, parameter)
+                
+        # Integer division
+        elif method == "//":
+            user_answer = input_valid_int(f"{parameter} // {table} = ", "Please enter a number. ")
+            correct_answer = run_math("//", table, parameter)
 
-            if answer !=  table * parameter:
-                print("No! Because of your bad math the zombies got you.")
-                eaten_by_zombies = True
-
-        #Integer division
-        elif method == "/":
-            answer = input_valid_float(f"{parameter} // {table} = ", "Please enter a number. ")
-
-            if answer != parameter // table:
-                print("No! Because of your bad math the zombies got you.")
-                eaten_by_zombies = True
-
-        #Modulus
+        # Modulus
         elif method == "%":
-            answer = input_valid_int(f"{parameter} % {table} = ", "Please enter a number. ")
+            user_answer = input_valid_int(f"{parameter} % {table} = ", "Please enter a number. ")
+            correct_answer = run_math("%", table, parameter)
 
-            if answer != parameter % table:
-                print("No! Because of your bad math the zombies got you.")
-                eaten_by_zombies = True
+        if user_answer != correct_answer:
+            print("No! Because of your bad math the zombies got you.")
+            sleep(2)
+            eaten_by_zombies = True
 
-
-        #If the player survived the math test, they get to choose a door.
+        # If the player survived the math test, they get to choose a door.
         if not eaten_by_zombies and remaining_doors > 1:
             door_with_zombie = random.randint(1, remaining_doors)
+            print(door_with_zombie)
             user_door = input_valid_int(f"You see {remaining_doors} doors, which one do you choose? ",
              f"Please enter a number between 1 and {remaining_doors}.", 1, remaining_doors)
 
             if door_with_zombie == user_door:
                 print("Zombies! You're dead.")
+                sleep(2)
                 eaten_by_zombies = True
             else:
                 print("Whew! No zombies.", end="\n\n") 
                 remaining_doors -= 1 
 
-        #If the player has gotten eaten by zombies, the round ends.
         if eaten_by_zombies:
-            sleep(2)
-            clear_screen()
-
-            answer = input_valid_str("Do you want to play again? y/n: ",
-                "Please choose y or n. ", ["y", "n"])
-            if answer == "n":
-                continue_playing = False
-
             break
 
-    #If the player survived the game.
+    clear_screen()
+
+    # If the player survived the game.
     if not eaten_by_zombies:
         print("You have survived the zombie house!")
         sleep(2)
         clear_screen()
+
+    answer = input_valid_str("Do you want to play again? y/n: ",
+        "Please choose y or n. ", ["y", "n"])
+    if answer == "n":
+        continue_playing = False
+    
+    clear_screen()
